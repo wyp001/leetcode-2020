5f94d31edcefe7c0348f6ddf277d534aa3590b47
wyupeng <1834583115@qq.com>
2022-07-19 16:42:14 +0800

    动态规划解决背包问题 代码格式化

--- src/main/java/com/wyp/leetcode/KnapsackProblem.java
+++ src/main/java/com/wyp/leetcode/KnapsackProblem.java
@@ -17,7 +17,7 @@ public class KnapsackProblem {
         //物品的个数
         int n = val.length;
         // 不同物品个数n、不同背包容量m下，背包可以装下物品的最大价值记录表
-        int[][] memo = new int[n+1][m+1];
+        int[][] memo = new int[n + 1][m + 1];
 
         for (int i = 0; i < n; i++) {
             memo[i][0] = 0;
@@ -27,16 +27,16 @@ public class KnapsackProblem {
         }
         for (int i = 1; i < memo.length; i++) { // 遍历行（物品重量）
             for (int j = 1; j < memo[i].length; j++) { // 遍历列（背包容量）j：背包容量
-                if (w[i-1] > j ) {
+                if (w[i - 1] > j) {
                     // 第i个（索引为i-1的）物品重量 大于 背包容量时，则此时背包所能装下物品的最大价值 为 可选物品个数 n-1,背包容量为 j 时 的最大价值
-                    memo[i][j] = memo[i-1][j];
+                    memo[i][j] = memo[i - 1][j];
                 } else {
                     // 第i个（索引为i-1的）物品重量 小于等于 背包容量时, 有两种方案
                     // 方案1：将第i个（索引为i-1的）物品 不放入背包，背包所能装下物品的最大价值 为 可选物品个数 n-1,背包容量为 j 时 的最大价值
-                    int v1 = memo[i-1][j];
+                    int v1 = memo[i - 1][j];
                     // 方案2：将第i个（索引为i-1的）物品 放入背包，背包的剩余容量为 [j - w[i - 1]]，再找 可选物品个数 n-1,背包容量为 [j - w[i - 1]] 时 的最大价值；
                     //       2个价值的和 即为 该方案下 背包所能放下物品的最大价值
-                    int v2 = val[i-1] + memo[i - 1][j - w[i - 1]];
+                    int v2 = val[i - 1] + memo[i - 1][j - w[i - 1]];
                     // 方案1 和 方案2 两种不同装法的最大值 即为 最终 背包所能放下物品的最大价值
                     memo[i][j] = Math.max(v1, v2);
                 }
