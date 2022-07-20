9beccc8126e8623e6779b1ebe90972cfa7239271
wyupeng <1834583115@qq.com>
2022-07-20 15:45:24 +0800

    动态规划  给你一个字符串 s，找到 s 中最长的回文子串。

--- src/main/java/com/wyp/leetcode/T5.java
+++ src/main/java/com/wyp/leetcode/T5.java
@@ -12,36 +12,69 @@ public class T5 {
     public static void main(String[] args) {
         T5 obj = new T5();
         String str = "babad";
-        str = "a";
+        // str = "a";
+        str = "esbtzjaaijqkgmtaajpsdfiqtvxsgfvijpxrvxgfumsuprzlyvhclgkhccmcnquukivlpnjlfteljvykbddtrpmxzcrdqinsnlsteonhcegtkoszzonkwjevlasgjlcquzuhdmmkhfniozhuphcfkeobturbuoefhmtgcvhlsezvkpgfebbdbhiuwdcftenihseorykdguoqotqyscwymtjejpdzqepjkadtftzwebxwyuqwyeegwxhroaaymusddwnjkvsvrwwsmolmidoybsotaqufhepinkkxicvzrgbgsarmizugbvtzfxghkhthzpuetufqvigmyhmlsgfaaqmmlblxbqxpluhaawqkdluwfirfngbhdkjjyfsxglsnakskcbsyafqpwmwmoxjwlhjduayqyzmpkmrjhbqyhongfdxmuwaqgjkcpatgbrqdllbzodnrifvhcfvgbixbwywanivsdjnbrgskyifgvksadvgzzzuogzcukskjxbohofdimkmyqypyuexypwnjlrfpbtkqyngvxjcwvngmilgwbpcsseoywetatfjijsbcekaixvqreelnlmdonknmxerjjhvmqiztsgjkijjtcyetuygqgsikxctvpxrqtuhxreidhwcklkkjayvqdzqqapgdqaapefzjfngdvjsiiivnkfimqkkucltgavwlakcfyhnpgmqxgfyjziliyqhugphhjtlllgtlcsibfdktzhcfuallqlonbsgyyvvyarvaxmchtyrtkgekkmhejwvsuumhcfcyncgeqtltfmhtlsfswaqpmwpjwgvksvazhwyrzwhyjjdbphhjcmurdcgtbvpkhbkpirhysrpcrntetacyfvgjivhaxgpqhbjahruuejdmaghoaquhiafjqaionbrjbjksxaezosxqmncejjptcksnoq";
         String res = obj.longestPalindrome(str);
         System.out.println(res);
     }
 
-    
+
+    /**
+     * 超时
+     * "esbtzjaaijqkgmtaajpsdfiqtvxsgfvijpxrvxgfumsuprzlyvhclgkhccmcnquukivlpnjlfteljvykbddtrpmxzcrdqinsnlsteonhcegtkoszzonkwjevlasgjlcquzuhdmmkhfniozhuphcfkeobturbuoefhmtgcvhlsezvkpgfebbdbhiuwdcftenihseorykdguoqotqyscwymtjejpdzqepjkadtftzwebxwyuqwyeegwxhroaaymusddwnjkvsvrwwsmolmidoybsotaqufhepinkkxicvzrgbgsarmizugbvtzfxghkhthzpuetufqvigmyhmlsgfaaqmmlblxbqxpluhaawqkdluwfirfngbhdkjjyfsxglsnakskcbsyafqpwmwmoxjwlhjduayqyzmpkmrjhbqyhongfdxmuwaqgjkcpatgbrqdllbzodnrifvhcfvgbixbwywanivsdjnbrgskyifgvksadvgzzzuogzcukskjxbohofdimkmyqypyuexypwnjlrfpbtkqyngvxjcwvngmilgwbpcsseoywetatfjijsbcekaixvqreelnlmdonknmxerjjhvmqiztsgjkijjtcyetuygqgsikxctvpxrqtuhxreidhwcklkkjayvqdzqqapgdqaapefzjfngdvjsiiivnkfimqkkucltgavwlakcfyhnpgmqxgfyjziliyqhugphhjtlllgtlcsibfdktzhcfuallqlonbsgyyvvyarvaxmchtyrtkgekkmhejwvsuumhcfcyncgeqtltfmhtlsfswaqpmwpjwgvksvazhwyrzwhyjjdbphhjcmurdcgtbvpkhbkpirhysrpcrntetacyfvgjivhaxgpqhbjahruuejdmaghoaquhiafjqaionbrjbjksxaezosxqmncejjptcksnoq"
+     */
+    // public String longestPalindrome(String s) {
+    //     int n = s.length();
+    //     Map<String, Integer> resMap = new HashMap<>();
+    //     for (int i = 1; i <= n; i++) {
+    //         for (int j = 0; j < i; j++) {
+    //             String s1 = s.substring(0, j);
+    //             String reverseS1 = new StringBuilder(s1).reverse().toString();
+    //             if (s1.startsWith(reverseS1)) {
+    //                 resMap.put(s1, s1.length());
+    //             }
+    //             String s2 = s.substring(j, i);
+    //             String reverseS2 = new StringBuilder(s2).reverse().toString();
+    //             if (s2.startsWith(reverseS2)) {
+    //                 resMap.put(s2, s2.length());
+    //             }
+    //         }
+    //     }
+    //     System.out.println(resMap);
+    //     String resStr = "";
+    //     for (String s1 : resMap.keySet()) {
+    //         if (s1.length() > resStr.length()) {
+    //             resStr = s1;
+    //         }
+    //     }
+    //     return resStr;
+    // }
+
+
+    //优化01 超时
     public String longestPalindrome(String s) {
         int n = s.length();
-        Map<String, Integer> resMap = new HashMap<>();
+        String resStr = "";
         for (int i = 1; i <= n; i++) {
             for (int j = 0; j < i; j++) {
                 String s1 = s.substring(0, j);
                 String reverseS1 = new StringBuilder(s1).reverse().toString();
                 if (s1.startsWith(reverseS1)) {
-                    resMap.put(s1, s1.length());
+                    if (s1.length() > resStr.length()) {
+                        resStr = s1;
+                    }
                 }
                 String s2 = s.substring(j, i);
                 String reverseS2 = new StringBuilder(s2).reverse().toString();
                 if (s2.startsWith(reverseS2)) {
-                    resMap.put(s2, s2.length());
+                    if (s2.length() > resStr.length()) {
+                        resStr = s2;
+                    }
                 }
-            }
-        }
-        System.out.println(resMap);
-        String resStr = "";
-        for (String s1 : resMap.keySet()) {
-            if (s1.length() > resStr.length()) {
-                resStr = s1;
             }
         }
         return resStr;
     }
+
+
 }
