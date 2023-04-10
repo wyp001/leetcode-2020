e39945a468ec787c3bc572782a32a38836f16b9b
wyupeng <1834583115@qq.com>
2023-04-10 14:22:56 +0800

    1796. 字符串中第二大的数字 update

--- src/main/java/com/wyp/leetcode/T1796.java
+++ src/main/java/com/wyp/leetcode/T1796.java
@@ -19,18 +19,38 @@ public class T1796 {
 
 
     public int secondHighest(String s) {
-        List<Integer> list = new ArrayList<>();
-        for (String e : s.split("")) {
-            if (e.matches("^\\d")) {
-                int num = Integer.parseInt(e);
-                if (!list.contains(num)) {
-                    list.add(num);
-                }
+        int max1 = -1;
+        int max2 = -1;
+        char[] chars = s.toCharArray();
+        for (char e : chars) {
+            if (Character.isLetter(e)) {
+                continue;
+            }
+            int num = e - '0';
+            if (num > max1) {
+                max2 = max1;
+                max1 = num;
+            } else if (num > max2 && num < max1) {
+                max2 = num;
             }
         }
-        if (list.size() >= 2) {
-            return list.stream().sorted().collect(Collectors.toList()).get(list.size() -2);
-        }
-        return -1;
+        return max2;
     }
+
+
+    // public int secondHighest(String s) {
+    //     List<Integer> list = new ArrayList<>();
+    //     for (String e : s.split("")) {
+    //         if (e.matches("^\\d")) {
+    //             int num = Integer.parseInt(e);
+    //             if (!list.contains(num)) {
+    //                 list.add(num);
+    //             }
+    //         }
+    //     }
+    //     if (list.size() >= 2) {
+    //         return list.stream().sorted().collect(Collectors.toList()).get(list.size() -2);
+    //     }
+    //     return -1;
+    // }
 }
